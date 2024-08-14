cf6d7b3a873b6784b70d7f74233ffba856d7a951
JennyPereira <jfpereira1@utpl.edu.ec>
2024-08-14 10:58:16 -0500

    starting new project with Angular

---
+++ 3. angular-project/simple-coffee-app/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  parrafo = 'Nuevo texto';
+  buttonState = false;
+
+  changeValue(): void {
+    if (this.parrafo == 'Nuevo texto') {
+      this.parrafo = 'Otro valor';
+    } else {
+      this.parrafo = 'Nuevo texto';
+    }
+  }
+}
