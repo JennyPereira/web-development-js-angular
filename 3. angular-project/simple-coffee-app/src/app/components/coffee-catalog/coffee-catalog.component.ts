72e14951b4dcbe862b63a0a290f8ea86d4bc0b07
JennyPereira <jfpereira1@utpl.edu.ec>
2024-08-16 08:48:42 -0500

    styles coffeeproduct

--- 3. angular-project/simple-coffee-app/src/app/components/coffee-catalog/coffee-catalog.component.ts
+++ 3. angular-project/simple-coffee-app/src/app/components/coffee-catalog/coffee-catalog.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { CoffeeService } from '../../services/coffee.service';
 import { CoffeeProduct } from '../../models/coffee-product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coffee-catalog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coffee-catalog.component.html',
   styleUrl: './coffee-catalog.component.css'
 })
 export class CoffeeCatalogComponent {
   coffeeProductList: CoffeeProduct[] = [];
 
-  constructor(private coffeeService: CoffeeService) { }
+  constructor(private coffeeService: CoffeeService) {
+    console.log(this.coffeeProductList);
+  }
 
   setCoffeeAll(): void {
     this.coffeeService.getAllCoffeeProducts()
       .subscribe(
         {
-          next: res => this.coffeeProductList = res,
+          next: res => console.log(res),
           error: err => console.log(),
         }
       );
---
+++ 3. angular-project/simple-coffee-app/src/app/components/coffee-product/coffee-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './coffee-product.component.html',
+  styleUrl: './coffee-product.component.css'
+})
+export class CoffeeProductComponent {
+
+}
