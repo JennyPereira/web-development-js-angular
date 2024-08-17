62b40899ca84da35a19cedad2ffad95cc0b549a3
JennyPereira <jfpereira1@utpl.edu.ec>
2024-08-17 10:25:24 -0500

    Adding routes

--- 3. angular-project/simple-coffee-app/src/app/app.component.ts
+++ 3. angular-project/simple-coffee-app/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { CoffeeCatalogComponent } from './components/coffee-catalog/coffee-catalog.component';
 import { CoffeeProductFormComponent } from './components/coffee-product-form/coffee-product-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CoffeeCatalogComponent, CoffeeProductFormComponent],
+  imports: [CoffeeCatalogComponent, CoffeeProductFormComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 3. angular-project/simple-coffee-app/src/app/app.routes.ts
+++ 3. angular-project/simple-coffee-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { CoffeeCatalogComponent } from './components/coffee-catalog/coffee-catalog.component';
+import { CoffeeProductFormComponent } from './components/coffee-product-form/coffee-product-form.component';
+import { CoffeeProductDetailComponent } from './components/coffee-product-detail/coffee-product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CoffeeCatalogComponent,
+        title: 'Simple Coffee'
+    },
+    {
+        path: 'newproduct',
+        component: CoffeeProductFormComponent,
+        title: 'Nuevo Producto'
+    },
+    {
+        path: 'detail',
+        component: CoffeeProductDetailComponent,
+        title: 'Detalle del producto'
+    }
+];
